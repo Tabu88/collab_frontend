48da34de8c116fe0ba3a5d44bcf1d8a3f759cd64
Dennis.Wabomba <dennis.wabomba8@gmail.com>
2024-07-18 14:14:02 +0300

    Updated

--- lib/pages/add_task.dart
+++ lib/pages/add_task.dart
@@ -1,11 +1,11 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
-
 import '../controller/dashboard_controller.dart';
+import '../helpers/color_utils.dart';
 import '../helpers/constants.dart';
+import '../helpers/date_utils.dart' as date_util;
 
 class AddTaskPage extends StatefulWidget {
   const AddTaskPage({super.key});
@@ -23,18 +23,17 @@ class _AddTaskPageState extends State<AddTaskPage> {
   List<Map<String, dynamic>> _items = [];
   List<TextEditingController> _controllers = [];
   List<String> choices = ["Design","Development","Coding","Meeting", "Office Time", "User Experience"];
+  late ScrollController scrollController;
+  List<DateTime> currentMonthList = List.empty();
+  DateTime currentDateTime = DateTime.now();
 
   @override
   void initState() {
+    currentMonthList = date_util.DateUtils.daysInMonth(currentDateTime);
+    currentMonthList.sort((a,b) => a.day.compareTo(b.day));
+    currentMonthList = currentMonthList.toSet().toList();
+    scrollController = ScrollController(initialScrollOffset: 70.0 * currentDateTime.day);
     super.initState();
-    // _controller.text = "";
-    // _controller.selection = TextSelection.fromPosition(
-    //   TextPosition(offset: _controller.text.length)
-    // );
-    //
-    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-    //   FocusScope.of(context).requestFocus(_focusNode);
-    // });
   }
 
   void _continue() {
@@ -190,12 +189,25 @@ class _AddTaskPageState extends State<AddTaskPage> {
             const SizedBox(height: 20,),
             //Date Time widget
             Text(
-              "Creation Task",
+              "Task Deadline",
               style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.w600,
                   color: Color(Constants.primaryBlack())
               ),
+            ),
+            Container(
+              height: 150,
+              width: double.infinity,
+              child: ListView.builder(
+                itemCount: currentMonthList.length,
+                  controller: scrollController,
+                  scrollDirection: Axis.horizontal,
+                  physics: const ClampingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return capsuleView(index);
+                  }),
             ),
             const SizedBox(height: 30,),
             Text(
@@ -399,6 +411,79 @@ class _AddTaskPageState extends State<AddTaskPage> {
               ),
             ),
           ],
+        ),
+      ),
+    );
+
+  }
+
+  Widget capsuleView(int index) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            currentDateTime = currentMonthList[index];
+          });
+        },
+        child: Container(
+          height: 140,
+          width: 60,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: (currentMonthList[index].day != currentDateTime.day)
+                  ? [
+                    Colors.white.withOpacity(0.8),
+                    Colors.white.withOpacity(0.7),
+                    Colors.white.withOpacity(0.6)
+                    ]
+                  : [
+                      HexColor("ED6184"),
+                      HexColor("EF315B"),
+                      HexColor("E2042D"),
+                    ],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(0.0, 0.1),
+              stops: const [0.0, 0.5, 1.0],
+              tileMode: TileMode.clamp
+            ),
+            borderRadius: BorderRadius.circular(40),
+            boxShadow: const [
+              BoxShadow(
+                blurRadius: 4,
+                color: Colors.black12,
+                offset: Offset(4, 4),
+                spreadRadius: 2
+              )
+            ]
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  currentMonthList[index].day.toString(),
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: (currentMonthList[index].day != currentDateTime.day)
+                        ?  HexColor("465876")
+                        : Colors.white
+                  ),
+                ),
+                Text(
+                  date_util.DateUtils.weekdays[currentMonthList[index].weekday -1],
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: (currentMonthList[index].day != currentDateTime.day)
+                        ? HexColor("465876")
+                        : Colors.white
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
